6a1cc2f5b26be1e103fdf2515aca213b6ce13b8c
Baron78x <nico78nb@gmail.com>
2022-06-15 11:55:36 +0200

    usless

--- src/Entity/District.php
+++ src/Entity/District.php
@@ -13,12 +13,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
  * @ORM\Entity(repositoryClass=DistrictRepository::class)
  */
 class District
-{
-    public function __toString()
-    {
-        return $this->name;
-    }
-    
+{   
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
