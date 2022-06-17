374f8282b30f1c549558b5abf95f037fb5783c68
Baron78x <nico78nb@gmail.com>
2022-06-17 15:39:23 +0200

    no push

--- src/Controller/Api/v1/ApiLoginController.php
+++ src/Controller/Api/v1/ApiLoginController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApiLoginController extends AbstractController
 {
     /**
-     * @Route("/api/login", name="app_api_login")
+     * @Route("/api/v1/login", name="login_check")
      */
     public function index(): JsonResponse
     {
+        
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/ApiLoginController.php',
